c6246aae1aebbe43a4a1993034c9bae4c0b35eb1
MigueSaint <amg.sosa@yavirac.edu.ec>
2025-11-26 10:14:35 -0500

    cambio 2

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ HTML = """
 
 <div class="container py-5">
     <div class="card shadow p-4" style="max-width: 600px; margin:auto;">
-        <h2 class="text-center mb-4">Proyecto AMSG DevOps Mejorado</h2>
+        <h2 class="text-center mb-4">Proyecto de Miguel Sosa DevOps final</h2>
 
         <form method="POST" action="/">
             <label class="form-label">Ingrese su nombre:</label>
